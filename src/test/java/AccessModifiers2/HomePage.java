a8d0cd9d22d83a2a9d44e0756a744f13e3b58fff
Ravi9676 <korniravikishor0323@gmail.com>
2022-04-30 20:16:51 +0530

    Adding isdisplayed command

--- src/test/java/AccessModifiers2/HomePage.java
+++ src/test/java/AccessModifiers2/HomePage.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
-    public void openurl(String url){
+    public boolean openurl(String url){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\korni\\IdeaProjects\\Selenium\\Drivers\\chromedriver.exe");
         ChromeDriver driver=new ChromeDriver();
         driver.get(url);
-        /*WebElement mobileslink= driver.findElement(By.xpath("//a[text()='Mobiles']"));
+        WebElement mobileslink= driver.findElement(By.xpath("//a[text()='Mobiles']"));
         boolean c=mobileslink.isDisplayed();
-        return c;*/
+        return c;
 
     }
 
